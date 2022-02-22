8b9f3ddc7d760b1689bb668722d7137477f208ef
andrewstech <info@andrewstech.me>
2022-02-22 15:45:45 +0000

    Update merge system

--- json-merge.py
+++ json-merge.py
@@ -2,19 +2,20 @@ import json
 import os
  
 # function to add to JSON
-def write_json(new_data, filename='store/index.json'):
-    with open(filename,'r+') as file:
+def write_json(filename='store/index.json'):
+    with open(filename,'r+') as file, open('template.json') as temp:
           # First we load existing data into a dict.
         file_data = json.load(file)
+        y = json.load(temp)
         # Join new_data with file_data inside emp_details
-        file_data.append(new_data)
+        file_data.append(y)
         # Sets file's current position at offset.
         file.seek(0)
         # convert back to json.
         json.dump(file_data, file, indent = 4)
  
     # python object to be appended
-y = os.environ.get('json')
+
 
      
-write_json(y)
+write_json()
